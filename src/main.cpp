23f4d2edd4b55eee7f5cc4295672c087329166c0
Stéphane Heudron <stephane.heudron@grenoble-inp.org>
2017-03-04 10:57:12 +0100

    Make easier the automation of several instances

--- src/main.cpp
+++ src/main.cpp
@@ -15,27 +15,45 @@
 #define NF 128 // Number of facilities
 #define NC 128 // Number of customers
 #define SEED 1 // Seed of the generation
-#define TIME_MAX 1. // maximal time of each execution in second
-#define TIME_MAX_TOTAL 100. // maximal total time allocated for a single set of parameters
+#define TIME_MAX_EACH 1. // Time of each execution in seconds
+#define TIME_MAX_TOTAL 100. // Time to spend with each set of parameters in seconds
 
-using realIndividual = GA::BinaryRepresentation<NF>;
-using redundantIndividual = GA::BinaryRepresentation<NF*3>;
+using Individual = GA::BinaryRepresentation<NF>;
 
 using Clock = std::chrono::high_resolution_clock;
 using Duration = std::chrono::duration<double>;
 
-template<class Ind>
-void plot(std::ostream &os, GA::Engine<Ind> &ga) {
-    constexpr size_t INITIAL_SIZE = 32;
+template<class Individual>
+void generate(double time_max_each, double time_max_total, std::string path, size_t initial_size,
+              GA::Objective<Individual> &objective,
+              GA::Crossover<Individual> &crossover,
+              GA::Mutation<Individual> &mutation,
+              GA::Selection<Individual> &selection) {
     constexpr unsigned int STEPS = 10;
 
-    auto start = Clock::now();
-    auto end = start + Duration(TIME_MAX);
+    GA::Engine<Individual> ga(objective, crossover, mutation, selection);
+    std::cout << "### Execution (" << path << ")" << std::endl;
+    std::ofstream file;
 
-    ga.initialize(INITIAL_SIZE);
-    os << Duration(Clock::now() - start).count() << " " << ga.getScore() << std::endl;
-    while (Clock::now() < end) {
-        os << Duration(Clock::now() - start).count() << " " << ga.step(STEPS) << std::endl;
+    unsigned int number_max = time_max_total / time_max_each;
+    for (unsigned int number = 0; number < number_max; number++) {
+        std::cout << (100 * number / number_max) << "% (" << number << "/" << number_max << ")\r" << std::flush;
+        file.open(path + std::to_string(number));
+        if (!file.is_open()) {
+            std::cerr << "Can't open file " + path + std::to_string(number) << std::endl;
+        } else {
+
+            auto start = Clock::now();
+            auto end = start + Duration(time_max_each);
+
+            ga.initialize(initial_size);
+            file << Duration(Clock::now() - start).count() << " " << ga.getScore() << std::endl;
+            while (Clock::now() < end) {
+                file << Duration(Clock::now() - start).count() << " " << ga.step(STEPS) << std::endl;
+            }
+
+            file.close();
+        }
     }
 }
 
@@ -43,7 +61,7 @@ int main() {
     srand((unsigned int) time(nullptr));
 
     FacilityLocation::Instance<NF> instance = FacilityLocation::Instance<NF>::randomMetricInstance(NC, SEED);
-    FacilityLocation::Objective<realIndividual> realObjective(instance);
+    FacilityLocation::Objective<Individual> realObjective(instance);
 //    instance.save("facility_instance_" + std::to_string(SEED));
 //    instance = FacilityLocation::Instance<NF>::load("facility_instance_" + std::to_string(SEED));
 
@@ -58,26 +76,20 @@ int main() {
         std::cout << "Best score estimated (< 1.61*opt): " << FacilityLocation::Solver<NF>::greedy(instance) << std::endl;
     }
 
-//    FacilityLocation::Objective<realIndividual> objective(realObjective);
-//    GA::DeadBitInsertion<realIndividual, redundantIndividual> objective(realObjective, NF / 2);
-    GA::DuplicateBits<realIndividual, redundantIndividual> objective(realObjective, 2);
-    GA::MultiPointCrossover<redundantIndividual> crossover(1);
-    GA::RandomMutation<redundantIndividual> mutation(2. / NF);
-    GA::ProbabilistSelection<redundantIndividual> selection;
-
-    GA::Engine<redundantIndividual> ga(objective, crossover, mutation, selection);
-
-    std::cout << "### Execution" << std::endl;
-    std::ofstream file;
-
-    constexpr unsigned int NUMBER_MAX = TIME_MAX_TOTAL / TIME_MAX;
-    for (unsigned int number = 0; number < NUMBER_MAX; number++) {
-        std::cout << (100 * number / NUMBER_MAX) << "% (" << number << "/" << NUMBER_MAX << ")\r" << std::flush;
-        file.open("plot1_" + std::to_string(number));
-        plot(file, ga);
-        file.close();
+    {
+        GA::MultiPointCrossover<Individual> crossover(1);
+        GA::RandomMutation<Individual> mutation(1. / NF);
+        GA::ProbabilistSelection<Individual> selection;
+        generate(TIME_MAX_EACH, TIME_MAX_TOTAL, "plot0_", 32, realObjective, crossover, mutation, selection);
     }
-
+    {
+        using redundantIndividual = GA::BinaryRepresentation<NF*3>;
+        GA::DuplicateBits<Individual, redundantIndividual> objective(realObjective, 0);
+        GA::MultiPointCrossover<redundantIndividual> crossover(1);
+        GA::RandomMutation<redundantIndividual> mutation(1. / NF);
+        GA::ProbabilistSelection<redundantIndividual> selection;
+        generate(TIME_MAX_EACH, TIME_MAX_TOTAL, "plot1_", 32, objective, crossover, mutation, selection);
+    }
     return 0;
 }
 
